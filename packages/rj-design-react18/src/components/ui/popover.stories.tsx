66a1137bd4782e9470c9c89f0e86b3d1eba101b3
赵小明 <994769060@qq.com>
2025-08-05 19:18:37 +0800

    popover ：基础功能实现

--- packages/rj-design-react18/src/components/ui/popover.stories.tsx
+++ packages/rj-design-react18/src/components/ui/popover.stories.tsx
@@ -1,74 +1,429 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
 import {
     Popover,
-    PopoverAnchor,
     PopoverContent,
+    PopoverDesc,
+    PopoverIcon,
+    PopoverTitle,
+    PopoverToolbar,
     PopoverTrigger,
 } from "@/components/ui/popover"
+import { PopoverClose, type PopoverProps } from "@radix-ui/react-popover";
 import type { Meta, StoryObj } from "@storybook/react-vite"
+import { InfoCirecledIcon } from "../icon/infoCirecledIcon";
+// import { CrossCircledIcon } from "@radix-ui/react-icons";
 
+interface ExtendedPopoverProps extends PopoverProps {
+    contentVariant: 'default';
+    contentArrowVariant: 'default';
+    contentSideOffset: number;
+    contentAlignOffset: number;
+    contentSide: "top" | "right" | "bottom" | "left",
+    contentAlign: "center" | "start" | "end",
+    grid: 'default' | 'icon'
+}
 
-
-const meta: Meta<typeof Popover> = {
+const meta: Meta<ExtendedPopoverProps> = {
     title: 'Components/Popover',
     tags: ['autodocs'],
     component: Popover,
     argTypes: {
-
-
+        contentVariant: {
+            control: { type: 'select' },
+            options: ['default'],
+        },
+        contentArrowVariant: {
+            control: { type: 'select' },
+            options: ['default'],
+        },
+        contentSideOffset: {
+            control: 'number',
+        },
+        contentAlignOffset: {
+            control: 'number',
+        },
+        contentSide: {
+            control: { type: 'select' },
+            options: ["top", "right", "bottom", "left"],
+        },
+        contentAlign: {
+            control: { type: 'select' },
+            options: ["center", "start", "end"],
+        },
+        grid: {
+            control: { type: 'select' },
+            options: ["default", "icon"],
+        }
     },
 }
 
 export default meta
 
-type Story = StoryObj<typeof Popover>
+type Story = StoryObj<ExtendedPopoverProps>
 
-// 主要状态
-import React, { useRef, useState, useEffect } from 'react';
-
-export const Primary: Story = {
-    render: () => {
-        const popoverRef = useRef<HTMLDivElement>(null);
-        const [popoverHeight, setPopoverHeight] = useState(0);
-
-        useEffect(() => {
-            if (popoverRef.current) {
-                // 获取PopoverContent的实际高度
-                const height = popoverRef.current.offsetHeight;
-                setPopoverHeight(height);
-            }
-        }, []);
-
+// 上状态
+export const Top: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'top',
+        contentAlign: 'center'
+    },
+    render: (args) => {
         return (
-            <div id='mmsd' className="flex items-center justify-center h-[200px]">
+            <div className="flex items-center justify-center h-[200px]">
                 <Popover>
                     <PopoverTrigger asChild>
-                        <Button id='button-1' className="relative">
-                            远程打开
-                            {/* 动态计算锚点位置 */}
-                            <PopoverAnchor asChild>
-                                <div
-                                    id='anchor-1'
-                                    className="absolute"
-                                    style={{
-                                        left: 0,
-                                        bottom: `calc(100% + ${popoverHeight}px)`
-                                    }}
-                                />
-                            </PopoverAnchor>
+                        <Button variant={"primary"}>
+                            没有锚点
                         </Button>
                     </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        <p>高度将被动态计算</p>
+                    </PopoverContent>
+                </Popover>
+            </div>
+        );
+    }
+}
 
-                    {/* 获取PopoverContent高度 */}
-                    <PopoverContent ref={popoverRef}>
-                        <p>我出现在锚定区域附近</p>
-                        {/* 示例内容 */}
-                        <div className="p-4 bg-gray-100 rounded">
-                            <p>内容区域</p>
-                            <p>高度将被动态计算</p>
-                        </div>
+// 下状态
+export const Bottom: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'bottom',
+        contentAlign: 'center'
+    },
+    render: (args) => {
+        return (
+            <div className="flex items-center justify-center h-[200px]">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"primary"}>
+                            没有锚点
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        <p>高度将被动态计算</p>
+                    </PopoverContent>
+                </Popover>
+            </div>
+        );
+    }
+}
+
+// 左状态
+export const Left: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'left',
+        contentAlign: 'center'
+    },
+    render: (args) => {
+        return (
+            <div className="flex items-center justify-center h-[200px]">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"primary"}>
+                            没有锚点
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        <p>高度将被动态计算</p>
+                    </PopoverContent>
+                </Popover>
+            </div>
+        );
+    }
+}
+
+// right状态
+export const Right: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'right',
+        contentAlign: 'center'
+    },
+    render: (args) => {
+        return (
+            <div className="flex items-center justify-center h-[200px]">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"primary"}>
+                            没有锚点
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        <p>高度将被动态计算</p>
+                    </PopoverContent>
+                </Popover>
+            </div>
+        );
+    }
+}
+//标题+内容
+export const ContentTitleAndDesc: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'right',
+        contentAlign: 'center'
+    },
+    render: (args) => {
+        return (
+            <div className="flex items-center justify-center h-[200px]">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"primary"}>
+                            没有锚点
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        {/* <PopoverIcon className="text-primary">
+                            <InfoCirecledIcon />
+                        </PopoverIcon> */}
+                        <PopoverTitle  >
+                            标题
+                        </PopoverTitle>
+                        {/* <PopoverClose>
+                            <Button variant='ghost'>
+                                <CrossCircledIcon />
+                            </Button>
+                        </PopoverClose> */}
+                        <PopoverDesc  >
+                            <p>这是一段内容</p>
+                        </PopoverDesc>
+                        {/* <PopoverToolbar  >
+                            <div className="flex gap-2 justify-end">
+                                <Button variant='primary' size={"md"}>
+                                    确认
+                                </Button>
+                                <Button variant='default' size={"md"}>
+                                    取消
+                                </Button>
+                            </div>
+                        </PopoverToolbar> */}
+                    </PopoverContent>
+                </Popover>
+            </div>
+        );
+    }
+}
+//图标+内容+描述
+export const ContentIconAndTitleAndDesc: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'right',
+        contentAlign: 'center',
+        grid: 'icon'
+    },
+    render: (args) => {
+        return (
+            <div className="flex items-center justify-center h-[200px]">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"primary"}>
+                            没有锚点
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        <PopoverIcon className="text-primary">
+                            <InfoCirecledIcon />
+                        </PopoverIcon>
+                        <PopoverTitle  >
+                            标题
+                        </PopoverTitle>
+                        {/* <PopoverClose>
+                            <Button variant='ghost'>
+                                <CrossCircledIcon />
+                            </Button>
+                        </PopoverClose> */}
+                        <PopoverDesc grid={args.grid} >
+                            <p>这是一段内容</p>
+                        </PopoverDesc>
+                        {/* <PopoverToolbar  >
+                            <div className="flex gap-2 justify-end">
+                                <Button variant='primary' size={"md"}>
+                                    确认
+                                </Button>
+                                <Button variant='default' size={"md"}>
+                                    取消
+                                </Button>
+                            </div>
+                        </PopoverToolbar> */}
+                    </PopoverContent>
+                </Popover>
+            </div>
+        );
+    }
+}
+
+//图标+内容+描述
+export const ContentIconAndTitleAndCloseAndDesc: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'right',
+        contentAlign: 'center',
+        grid: 'icon'
+    },
+    render: (args) => {
+        return (
+            <div className="flex items-center justify-center h-[200px]">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"primary"}>
+                            没有锚点
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        <PopoverIcon className="text-primary">
+                            <InfoCirecledIcon />
+                        </PopoverIcon>
+                        <PopoverTitle  >
+                            标题
+                        </PopoverTitle>
+                        <PopoverClose>
+                            <Button variant='ghost'>
+                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7 14C10.866 14 14 10.866 14 7C14 3.13401 10.866 0 7 0C3.13401 0 0 3.13401 0 7C0 10.866 3.13401 14 7 14ZM10.046 4.71554L7.7615 7.00004L10.046 9.28454L9.2845 10.046L7 7.76154L4.7155 10.046L3.954 9.28454L6.2385 7.00004L3.954 4.71554L4.7155 3.95405L7 6.23854L9.2845 3.95405L10.046 4.71554Z" fill="#97A7B5" />
+                                </svg>
+                            </Button>
+                        </PopoverClose>
+                        <PopoverDesc grid={args.grid} >
+                            <p>这是一段内容</p>
+                        </PopoverDesc>
+                        {/* <PopoverToolbar  >
+                            <div className="flex gap-2 justify-end">
+                                <Button variant='primary' size={"md"}>
+                                    确认
+                                </Button>
+                                <Button variant='default' size={"md"}>
+                                    取消
+                                </Button>
+                            </div>
+                        </PopoverToolbar> */}
+                    </PopoverContent>
+                </Popover>
+            </div>
+        );
+    }
+}
+
+//内容部分
+export const ContentIconAndTitleAndCloseAndDescAndToolbar: Story = {
+    args: {
+        contentVariant: 'default',
+        contentArrowVariant: 'default',
+        contentSideOffset: 0,
+        contentAlignOffset: 0,
+        contentSide: 'right',
+        contentAlign: 'center',
+        grid: 'icon'
+    },
+    render: (args) => {
+        return (
+            <div className="flex items-center justify-center h-[200px]">
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"primary"}>
+                            没有锚点
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        variant={args.contentVariant}
+                        arrow={args.contentArrowVariant}
+                        side={args.contentSide}
+                        align={args.contentAlign}
+                        alignOffset={args.contentAlignOffset}
+                        sideOffset={args.contentSideOffset}>
+                        <PopoverIcon className="text-primary">
+                            <InfoCirecledIcon />
+                        </PopoverIcon>
+                        <PopoverTitle  >
+                            标题
+                        </PopoverTitle>
+                        <PopoverClose>
+                            <Button variant='ghost'>
+                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7 14C10.866 14 14 10.866 14 7C14 3.13401 10.866 0 7 0C3.13401 0 0 3.13401 0 7C0 10.866 3.13401 14 7 14ZM10.046 4.71554L7.7615 7.00004L10.046 9.28454L9.2845 10.046L7 7.76154L4.7155 10.046L3.954 9.28454L6.2385 7.00004L3.954 4.71554L4.7155 3.95405L7 6.23854L9.2845 3.95405L10.046 4.71554Z" fill="#97A7B5" />
+                                </svg>
+                            </Button>
+                        </PopoverClose>
+                        <PopoverDesc grid={args.grid} >
+                            <p>这是一段内容</p>
+                        </PopoverDesc>
+                        <PopoverToolbar  >
+                            <div className="flex gap-2 justify-end">
+                                <Button variant='primary' size={"md"}>
+                                    确认
+                                </Button>
+                                <Button variant='default' size={"md"}>
+                                    取消
+                                </Button>
+                            </div>
+                        </PopoverToolbar>
                     </PopoverContent>
                 </Popover>
             </div>
--- packages/rj-design-react18/src/components/ui/popover.tsx
+++ packages/rj-design-react18/src/components/ui/popover.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import * as PopoverPrimitive from "@radix-ui/react-popover"
 
 import { cn } from "@/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority"
 
 function Popover({
   ...props
@@ -15,26 +16,52 @@ function PopoverTrigger({
   return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />
 }
 
+const popoverContentVariants = cva(
+  '',
+  {
+    variants: {
+      variant: {
+        default: 'bg-fill-light-hover-bg text-text',
+      },
+      arrow: {
+        default: 'bg-fill-light-hover-bg fill-fill-light-hover-bg',
+      }
+    },
+    defaultVariants: {
+    },
+  })
+
 function PopoverContent({
   className,
-  align = "start",
+  variant,
+  arrow,
+  side = "left",
+  align = "center",
   sideOffset = 0,
-  alignOffset = 8,
+  alignOffset = 0,
+  children,
   ...props
-}: React.ComponentProps<typeof PopoverPrimitive.Content>) {
+}: React.ComponentProps<typeof PopoverPrimitive.Content>
+  & VariantProps<typeof popoverContentVariants>) {
   return (
     <PopoverPrimitive.Portal>
       <PopoverPrimitive.Content
         data-slot="popover-content"
+        side={side}
         align={align}
         alignOffset={alignOffset}
         sideOffset={sideOffset}
         className={cn(
-          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden",
-          className
+          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden grid grid-cols-[auto_1fr_auto] gap-x-2 gap-y-2",
+          className,
+          popoverContentVariants({ variant })
         )}
         {...props}
-      />
+      >
+        {children}
+        <PopoverPrimitive.Arrow className={cn("bg-primary fill-primary z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]",
+          popoverContentVariants({ arrow }))} />
+      </PopoverPrimitive.Content>
     </PopoverPrimitive.Portal>
   )
 }
@@ -45,4 +72,121 @@ function PopoverAnchor({
   return <PopoverPrimitive.Anchor data-slot="popover-anchor" {...props} />
 }
 
-export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
+interface PopoverCustomProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  grid?: 'default' | 'icon'
+}
+
+//自定义的Content 用来定位
+//前置图标
+function PopoverIcon({
+  children,
+  className,
+  ...props
+}: PopoverCustomProps) {
+  return (
+    <div
+      data-slot="popover-icon"
+      className={cn('col-start-1 row-start-1 inline-flex items-center justify-center', className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+//标题
+function PopoverTitle({
+  children,
+  className,
+  ...props
+}: PopoverCustomProps) {
+  return (
+    <div
+      data-slot="popover-title"
+      className={cn('col-start-2 row-start-1 inline-flex items-center justify-start text-text-deep text-[15px] leading-[22px] font-normal', className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+//关闭
+function PopoverClose({
+  children,
+  className,
+  ...props
+}: PopoverCustomProps) {
+  return (
+    <div
+      data-slot="popover-close"
+      className={cn('col-start-3 row-start-1 inline-flex items-center justify-end', className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+const popoverDescVariants = cva(
+  '',
+  {
+    variants: {
+      grid: {
+        default: 'col-start-2 row-start-2',
+        icon: 'col-span-3 row-start-2',
+      },
+
+    },
+    defaultVariants: {
+    },
+  })
+
+
+//关闭
+function PopoverDesc({
+  children,
+  className,
+  grid = 'default',
+  ...props
+}: PopoverCustomProps) {
+  return (
+    <div
+      data-slot="popover-desc"
+      className={cn('inline-flex items-center justify-start text-secondary', className, popoverDescVariants({ grid }))}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+
+//工具栏
+function PopoverToolbar({
+  children,
+  className,
+  ...props
+}: PopoverCustomProps) {
+  return (
+    <div
+      data-slot="popover-toolbar"
+      className={cn('col-span-3 row-start-3 inline-flex items-center justify-end', className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverAnchor,
+  PopoverIcon,
+  PopoverTitle,
+  PopoverClose,
+  PopoverDesc,
+  PopoverToolbar
+}
